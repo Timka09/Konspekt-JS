718bf8afd1ba3df4f34874b984c0909fcc599802
Timka09 <tymofij.i.naumenko@gmail.com>
2025-01-19 12:14:34 +0200

    OOP upd 1

--- js/Objects/OOP.js
+++ js/Objects/OOP.js
@@ -1,6 +1,14 @@
-// Прототипне наслідування - це коли ми створюємо обєкт(екземплям) та екземпляр унасліду,
-// всі властивіості і методи від прототипа
+// OOP - це метод написання коду через обєкти
+// Процедурне генерування - це написання коду через незвязані між собою функції та змінні(що в майбутньому може заважати та незиіватись spaghetti code)
 
+// Прототипне наслідування - це коли ми створюємо обєкт(екземплям). Eкземпляр унаслідує, всі властивіості і методи від прототипа
+
+
+// hasOwnProperty() - перевіряє чи має зазначений обєкт свій елемент (та повертає тру або фолс)
+
+// isPrototypeOf() - перевіряє чи зазначений єкземпляр є зазначеним прототипом
+
+// прототип
 const user = {
     site: "youtube",
     status: "standart",
@@ -9,7 +17,7 @@ const user = {
     }
 }
 // створюємо обєкт на базі юзеру ^
-// прототип
+// єкземпляр
 const userMark = Object.create(user)
 console.dir(Object)
 console.log(userMark.site)
@@ -17,3 +25,119 @@ console.log(userMark.showInfo())
 console.log(userMark)
 userMark.login = "Tima"
 userMark.password = 8765432
+
+for (const key in userMark) {
+    if (userMark.hasOwnProperty(key)) {
+        console.log(key);
+    }
+}
+
+// Функція конструктор - це модель або шаблон, за яким ми створюємо екземпляр
+// передамо аргументи у вигляді обєкта параметрів
+const Car = function ({ modelCar, priceCar, colorCar}) {
+  // const {modelCar, priceCar, colorCar} = infoBMW
+  this.model = modelCar;
+  this.cost = priceCar;
+  this.color = colorCar;
+  this.showInfo = function () {
+        return `автомобіль ${this.model} коштує ${this.cost} та має колір ${this.color}`
+    }
+};
+
+const infoBMW = {
+    modelCar: "BMW",
+    priceCar: 6000,
+    colorCar: "white"
+}
+
+const carBMW = new Car(infoBMW)
+console.log(carBMW)
+
+const carLamba = new Car({
+    modelCar: "Lamborgini",
+    priceCar: 10000000,
+    colorCar: "blue"
+})
+console.log(carLamba)
+
+console.log(carBMW.showInfo())
+console.log(carLamba.showInfo());
+
+// Напишемо гру битва орків та елфів
+class Hero {
+
+    constructor(infoOrk) {
+        this.name = infoOrk.heroName
+        this.points = infoOrk.heroPoints;
+        this.power = infoOrk.heroPower;
+    }
+
+    showInfo() {
+        return `Герой ${this.name} має стільки поїнтів ${this.points} та стільки сили ${this.power}`
+    }
+}
+
+class Ork extends Hero {
+    constructor(infoOrk) {
+        super(infoOrk) // Цей метод затягує данні з конструктора класу Hero
+        this.skin = infoOrk.heroSkin
+        this.iq = infoOrk.heroPoints
+    }
+
+    orkAttacks() {
+        return `Вас атакує орк з iq ${this.iq}`
+    }
+}
+
+const broOrk = new Ork({
+    heroName: "Bro",
+    heroPoints: 10,
+    heroPower: 20,
+    heroSkin: "green",
+    heroIq: 4
+})
+console.log(broOrk)
+
+const alexOrk = new Ork({
+  heroName: "Alex",
+  heroPoints: 12,
+  heroPower: 22,
+  heroSkin: "green",
+  heroIq: 6,
+});
+console.log(alexOrk)
+
+class Elf extends Hero {
+    constructor(infoElf) {
+        super(infoElf)
+        this.earsLength = infoElf.heroEarsLength
+        this.age = infoElf.heroAge
+    }
+
+    elfPowers() {
+        return `Ельф виходить до бою з такою кількістью років ${this.age}`
+    }
+}
+
+const johnElf = new Elf({
+    heroName: "John",
+    heroPoints: 15,
+    heroPower: 10,
+    heroEarsLength: "15см",
+    heroAge: 500
+})
+
+
+const bredElf = new Elf({
+  heroName: "Bred",
+  heroPoints: 15,
+  heroPower: 10,
+  heroEarsLength: "15см",
+  heroAge: 500,
+});
+console.log(bredElf)
+
+console.log(alexOrk.showInfo())
+console.log(alexOrk.orkAttacks())
+
+console.log(johnElf.elfPowers())
